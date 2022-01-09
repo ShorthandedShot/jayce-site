84c9fbd0e28948b2a12d2bfd6414de8e4e961345
Jayce Nguyen <32528654+ShorthandedShot@users.noreply.github.com>
2022-01-09 18:43:38 -0500

    help

--- js/functions.js
+++ js/functions.js
@@ -1,19 +1,5 @@
 $(document).ready(function(){
 
-  // GRID GRID GRID
-     var $grid = $('.grid').masonry({
-       itemSelector: '.box',
-       columnWidth: '.box-sizer2',
-       gutter: 32,
-       // horizontalOrder: true,
-       isFitWidth: true,
-       transitionDuration: '0.2s'
-     });
-     // layout Masonry after each image loads
-     $grid.imagesLoaded().progress( function() {
-       $grid.masonry('layout');
-     });
-
     // LIGHTBOX LIGHTBOX
     $('.popup').magnificPopup({
       delegate: 'a',
@@ -26,55 +12,17 @@ $(document).ready(function(){
 
     });
 
-  var $slider = $('#landing-slideshow').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2500,
-    speed: 0,
-    cssEase: 'linear',
-    fade: true,
-    infinite: true,
-    prevArrow: false,
-    nextArrow: false,
-    // lazyLoad: 'progressive',
-    focusOnSelect: false,
-  });
-
-  $slider.find(".slick-slide").on("click", function(){
-     $slider.slick("slickNext");
+// GRID GRID GRID
+   var $grid = $('.grid').masonry({
+     itemSelector: '.box',
+     columnWidth: '.box-sizer2',
+     gutter: 32,
+     // horizontalOrder: true,
+     isFitWidth: true,
+     transitionDuration: '0.2s'
    });
-
-  var $slider2 = $('.pic').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    speed: 0,
-    cssEase: 'linear',
-    fade: true,
-    infinite: true,
-    prevArrow: false,
-    nextArrow: false,
-    // lazyLoad: 'progressive',
-  });
-
-  $slider2.find(".slick-slide").on("click", function(){
-     $slider2.slick("slickNext");
+   // layout Masonry after each image loads
+   $grid.imagesLoaded().progress( function() {
+     $grid.masonry('layout');
    });
-
-   var $slider3 = $('.mini-slideshow').slick({
-     slidesToShow: 1,
-     slidesToScroll: 1,
-     speed: 0,
-     cssEase: 'linear',
-     fade: true,
-     infinite: true,
-     prevArrow: false,
-     nextArrow: false,
-     // lazyLoad: 'progressive',
-   });
-
-   $slider3.find(".slick-slide").on("click", function(){
-      $slider3.slick("slickNext");
-    });
-
 });
