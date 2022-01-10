db1ef12e70b7fac57706722517d4ee3c6703203d
Jayce Nguyen <32528654+ShorthandedShot@users.noreply.github.com>
2022-01-09 19:25:31 -0500

    AHHHHHHHHHHH

--- js/functions.js
+++ js/functions.js
@@ -1,32 +1,34 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
   // GRID GRID GRID
-     var $grid = $('.grid').masonry({
-       itemSelector: '.box',
-       columnWidth: '.box-sizer2',
-       gutter: 32,
-       // horizontalOrder: true,
-       isFitWidth: true,
-       transitionDuration: '0.2s'
-     });
-     // layout Masonry after each image loads
-     $grid.imagesLoaded().progress( function() {
-       $grid.masonry('layout');
-     });
 
-    // LIGHTBOX LIGHTBOX
-    $('.popup').magnificPopup({
-      delegate: 'a',
-      type:'image',
-      gallery: {
-        enabled: true
-      },
-      showCloseBtn: true,
-      closeOnContentClick: true,
+  var $grid = $('.grid').masonry({
+    itemSelector: '.box',
+    columnWidth: '.box-sizer2',
+    gutter: 32,
+    // horizontalOrder: true,
+    isFitWidth: true,
+    transitionDuration: '0.2s'
+  });
+  // layout Masonry after each image loads
+  $grid.imagesLoaded().progress(function() {
+    $grid.masonry('layout');
+  });
 
-    });
+  // LIGHTBOX LIGHTBOX
+  $('.popup').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery: {
+      enabled: true
+    },
+    showCloseBtn: true,
+    closeOnContentClick: true,
+
+  });
+
 });
 
-$(window).on('load', function(){
-  $grid.masonry('layout');
-  });
+jQuery(document).on('lazyloaded', function(e) {
+  jQuery('.grid').masonry({}).masonry('layout');
+});
