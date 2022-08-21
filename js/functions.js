dc0ffdb606c0738df7518c6cf24a0a1c914f52b0
Jayce Nguyen <32528654+ShorthandedShot@users.noreply.github.com>
2022-08-21 01:25:56 -0400

    unsportsmanlike done

--- js/functions.js
+++ js/functions.js
@@ -49,7 +49,7 @@ $(document).ready(function() {
       // close: false,
       bgOpacity: 0.8,
       bgClickAction: 'close',
-      padding: { top: 40, bottom: 40, left: 80, right: 80 },
+      padding: { top: 25, bottom: 25, left: 50, right: 50 },
 
       mouseMovePan: true,
 
