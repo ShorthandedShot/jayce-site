299fa4580f55efb57e1d84408449171d137314e6
Jayce Nguyen <32528654+ShorthandedShot@users.noreply.github.com>
2022-09-08 18:12:38 -0400

    hehehe

--- js/functions.js
+++ js/functions.js
@@ -70,5 +70,78 @@ $(document).ready(function() {
     var lightbox = new PhotoSwipeLightbox(options);
     lightbox.init();
 
-
+    $(".role").fadeIn(1000);
 });
+
+//Changing words
+
+var words = [
+  //List words here
+  'multidisciplinary designer',
+  'art director'
+],
+    currentStep = 0,
+    textEl =
+
+
+    document.querySelector('.change-text'),
+    oldWord = '';
+
+
+setTimeout(changeWord, 50);
+
+function changeWord() {
+  oldWord = textEl.innerHTML;
+
+  // check if there is a word atm or not
+  if (oldWord.length < 1) {
+
+    if (currentStep !== words.length -1) {
+          currentStep ++;
+    }else {
+      currentStep = 0;
+    }
+    addNextWord();
+  } else {
+    setTimeout(deleteWord, 5000);
+  }
+
+};
+
+function deleteWord() {
+  var WordLength = oldWord.length,
+      currentWord = textEl.innerHTML,
+      currentLength = currentWord.length;
+
+
+  // The word is deleted so, start adding in the new one
+  if (currentLength < 1) {
+    changeWord();
+    return;
+  }
+
+  // Remove a charachter
+  textEl.innerHTML = currentWord.substring(0, currentLength - 1);
+
+  setTimeout(deleteWord, 50);
+}
+
+function addNextWord() {
+  var currentWord = textEl.innerHTML,
+      currentLength = currentWord.length,
+      nextWord = words[currentStep],
+      nextWordLength = nextWord.length;
+
+
+  if (currentLength === nextWordLength) {
+    changeWord();
+    return;
+  }
+
+  // add a charachter
+  textEl.innerHTML = nextWord.substring(0, currentLength + 1);
+
+  setTimeout(addNextWord, 50);
+
+
+}
